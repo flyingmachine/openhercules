1c0182a7416c60de6da8ed18e7947e1bbd3c3876
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-04-25 07:57:40 -0400

    updated prod to use mongohq url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Checklisthub::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.after_initialize do
+    MongoThing.db = ENV['MONGOHQ_URL']
+  end
 end
