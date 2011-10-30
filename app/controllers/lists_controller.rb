fe7d5feacc34aa98394058f8451a7f7afb14e218
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-10-30 08:35:30 -0400

    updated gemfile

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => :show
    
   def index
     if current_user.lists.empty?
