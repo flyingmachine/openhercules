f00f2a65db3b102eacabb77688bb473ca2b20685
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-11-05 08:38:05 -0400

    specs for username like

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,18 @@ describe User do
 
   let(:user) { FactoryGirl.create(:user) }
   let(:user2) { FactoryGirl.create(:user) }
+  
+  describe ".like" do
+    it "should only return users where the username starts with the given text" do
+      u1 = FactoryGirl.create(:user, :username => "george")
+      u2 = FactoryGirl.create(:user, :username => "benny")
+      
+      User.username_like("geo").should == [u1]
+      User.username_like("ben").should == [u2]
+    end
+  end
 
-  describe ".receive_list" do
+  describe "#receive_list" do
     it "should add a list to list invitations if the user doesn't already have the list" do
       list = List.create_default(user)
       user2.receive_list(list, User::LIST_PERMISSIONS[0])
