fbb116e081d6b1fd41135836f185ec6d8492e2b9
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-12-09 19:21:25 -0500

    more cuke stuff

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,28 +5,18 @@ class Ability
     user ||= User.new
 
     can :read, List, :global_permission => ListPermissions::READ
-    
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    can :read, List, :global_permission => ListPermissions::WRITE
+    can :read, List, :user_id => user.id
+    can :read, List do |list|
+      list.sharees.include? User
+    end
+
+    can :modify_items, List, :global_permission => ListPermissions::WRITE
+    can :modify_items, List, :user_id => user.id
+    can :modify_items, List do |list|
+      list.writers.include? user.id.to_s
+    end
+
+    can :modify_properties, List, :user_id => user.id
   end
 end
